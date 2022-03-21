c3eac888a74b8d61621a379df954b6bed09ed929
Arun <arunjoseph001@gmail.com>
2022-03-21 17:42:19 +0530

    Updated all the project files

--- script.js
+++ script.js
@@ -12,43 +12,48 @@ const form = document.querySelector("#popupform");
 
 function printBooks() {
   books.forEach((myBook) => {
-    console.log("The myBook is"+ myBook);
+    // console.log("The myBook is"+ myBook);
     printBook(myBook);
   });
 }
 printBooks();
 
 function printBook(book) {
-  console.log("The book is "+ book);
+  // console.log("The book is "+ book);
   // console.log(`${book[title]} is written by ${book[author]} and has ${book[pages]} and reading status is ${book[read]}`);
   let myDiv = document.createElement("div");
   myDiv.classList.add("book");
-  let newBook =
-    "<h3>" +
-    book.title +
-    "</h3><p>" +
-    book.author +
-    "</p><p>" +
-    book.pages +
-    "</p><p>" +
-    book.read +
-    "</p>";
+  // let newBook =
+  //   "<h3>" +
+  //   book.title +
+  //   "</h3><p>" +
+  //   book.author +
+  //   "</p><p>" +
+  //   book.pages +
+  //   "</p><p>" +
+  //   book.read +
+  //   "</p>";
+  let newBook = "";
+  for (let key in book) {
+    // console.log(`${key}: ${book[key]}`);
+    newBook = newBook + `<p>${book[key]}</p>`;
+  }
   myDiv.innerHTML = newBook;
-  console.log("The newBook is " + newBook);
-  console.log("The myDiv is " + myDiv);
+  // console.log("The newBook is " + newBook);
+  // console.log("The myDiv is " + myDiv);
   document.querySelector(".container").appendChild(myDiv);
-  // document.querySelector('.container').appendChild(document.createElement("div"))
-  // for (let key in book) {
-  //   console.log(`${key}: ${book[key]}`);
-  // }
 }
 function formSubmit(e) {
   e.preventDefault();
   const data = new FormData(e.target);
   const bookForm = Object.fromEntries(data.entries());
   //   console.log(bookForm);
+  document.querySelector(".bookForm").style.display = "none";
   books.push(bookForm);
   printBook(bookForm);
 }
 
 form.addEventListener("submit", formSubmit);
+span.addEventListener("click", () => {
+  document.querySelector(".bookForm").style.display = "block";
+});
